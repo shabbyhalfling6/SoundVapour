4cae460e6233f9c10b95351173d4d2da32245d24
shabbyhalfling6 <bradycjohnston@hotmail.com>
2017-04-05 14:32:20 +1000

    Scripted Player Input and shape vapourising

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public string selectedShape;
+
+    private float horizontalDialSelection;
+    private float verticalDialSelection;
+
+
+
+
+    void Update ()
+    {
+        horizontalDialSelection = Input.GetAxis("Horizontal");
+        verticalDialSelection = Input.GetAxis("Vertical");
+
+        if (horizontalDialSelection < 0)
+            selectedShape = "Square";
+        else if (horizontalDialSelection > 0)
+            selectedShape = "Circle";
+        if (verticalDialSelection < 0)
+            selectedShape = "X";
+        else if (verticalDialSelection > 0)
+            selectedShape = "Triangle";
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        //if the shape passing the line is the selected shape and you press the vapourise button...
+        if(selectedShape == collision.gameObject.tag)
+        {
+            Debug.Log("Collision detected: " + collision.tag);
+            //and you press the vapourise button...
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                //destroy the shape
+                Destroy(collision.gameObject);
+            }
+        }
+    }
 }
