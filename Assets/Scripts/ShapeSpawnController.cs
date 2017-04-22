a03344dda8a4eeb5b17e18f774d5a564aae86ac4
shabbyhalfling6 <bradycjohnston@hotmail.com>
2017-04-23 01:52:09 +1000

    Updated Shape Spawner
    Now has more variables used to edit the behaviour of the shapes spawning
    - Each shapes has its own spawn rate, spawn threshold, and min and max
    spawn areas for which parts of the song spawn each shape

--- Assets/Scripts/ShapeSpawnController.cs
+++ Assets/Scripts/ShapeSpawnController.cs
@@ -7,60 +7,82 @@ public class ShapeSpawnController : MonoBehaviour {
     public GameObject[] shapes = new GameObject[4];
     public Transform[] shapeSpawners = new Transform[4];
 
-    private float spawnTimer = 0.0f;
-    public float spawnThreshold = 0.17f;
+    private float squareSpawnTimer = 0.45f;
+    private float circleSpawnTimer = 0.45f;
+    private float triangleSpawnTimer = 0.45f;
+    private float xSpawnTimer = 0.45f;
 
-    public float squareSpawnMax = 4;
-    public float triangleSpawnMax = 8;
-    public float circleSpawnMax = 12;
+    public float squareSpawnThreshold = 0.17f;
+    public float circleSpawnThreshold = 0.17f;
+    public float triangleSpawnThreshold = 0.17f;
+    public float xSpawnThreshold = 0.17f;
 
-    public float spawnRate = 0.45f;
+    private int square = 0;
+    private int circle = 1;
+    private int triangle = 2;
+    private int x = 3;
+
+    public int squareSpawnMin = 1;
+    public int squareSpawnMax = 4;
+    public int triangleSpawnMin = 3;
+    public int triangleSpawnMax = 8;
+    public int circleSpawnMin = 7;
+    public int circleSpawnMax = 12;
+    public int xSpawnMin = 11;
+    public int xSpawnMax = 13;
+
+    public float squareSpawnRate = 0.45f;
+    public float circleSpawnRate = 0.45f;
+    public float triangleSpawnRate = 0.45f;
+    public float xSpawnRate = 0.45f;
 
     public AudioSource sampleTrack;
 
+    float[] spectrum = new float[512];
+    float[] spawnTimers;
+
+    void Start()
+    {
+        spawnTimers = new float[4] { squareSpawnTimer, circleSpawnTimer, triangleSpawnTimer, xSpawnTimer };
+    }
+
     void Update ()
     {
-        spawnTimer -= Time.deltaTime;
+        for(int i = 0; i < spawnTimers.Length; i++)
+        {
+            spawnTimers[i] -= Time.deltaTime;
+        }
 
-        float[] spectrum = new float[512];
-
+        //populate the spectrum array with the window date from current audio source
         sampleTrack.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
-        if (spawnTimer <= 0)
-        {
 
-            for (int i = 1; i < spectrum.Length - 1; i++)
+        //Call the spawn shape function for each shape
+        SpawnShapes(square, squareSpawnRate, squareSpawnMin, squareSpawnMax, squareSpawnThreshold);
+        SpawnShapes(circle, circleSpawnRate, circleSpawnMin, circleSpawnMax, circleSpawnThreshold);
+        SpawnShapes(triangle, triangleSpawnRate, triangleSpawnMin, triangleSpawnMax, triangleSpawnThreshold);
+        SpawnShapes(x, xSpawnRate, xSpawnMin, xSpawnMax, xSpawnThreshold);
+    }
+
+    void SpawnShapes(int shapeType, float spawnRate, int shapeSpawnMin, int shapeSpawnMax, float spawnThreshold)
+    {
+        //checks if the type of shape is ready to spawn yet
+        if (spawnTimers[shapeType] <= 0)
+        {
+            //loop through the audio source data in the array between the min and max the shape spawns for
+            for (int i = shapeSpawnMin; i < shapeSpawnMax; i++)
             {
+                //checks if the audio has peaked above a certain level
                 if (spectrum[i] >= spawnThreshold)
                 {
-                    if (i <= squareSpawnMax)
-                    {
-                        Instantiate(shapes[0], shapeSpawners[0].position, shapeSpawners[0].rotation);
-                        spawnTimer = spawnRate;
-                        break;
-                    }
-                    else if (i <= triangleSpawnMax)
-                    {
-                        Instantiate(shapes[1], shapeSpawners[1].position, shapeSpawners[1].rotation);
-                        spawnTimer = spawnRate;
-                        break;
-                    }
-                    else if (i <= circleSpawnMax)
-                    {
-                        Instantiate(shapes[2], shapeSpawners[2].position, shapeSpawners[2].rotation);
-                        spawnTimer = spawnRate;
-                        break;
-
-                    }
-                    else
-                    {
-                        Instantiate(shapes[3], shapeSpawners[3].position, shapeSpawners[3].rotation);
-                        spawnTimer = spawnRate;
-                        break;
-                    }
+                    //spawns the shape type
+                    Instantiate(shapes[shapeType], shapeSpawners[shapeType].position, shapeSpawners[shapeType].rotation);
+                    //set this shape types spawn timer back
+                    spawnTimers[shapeType] = spawnRate;
+                    //break the loop
+                    break;
                 }
             }
-
         }
-	}
+    }
 }
